3bc43314b568be203a2b8f8cf9c28a9f559d74a7
Timothy Brown <tbbrown@wustl.edu>
2015-12-15 11:53:49 -0600

    Added CheckForUpdateFixPackageCompletion

---
+++ CompletionCheck_UpdateFixPackage/CheckForUpdateFixPackageCompletion.HCP_Staging.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ -z "${SUBJECT_FILES_DIR}" ]; then
+	echo "Environment variable SUBJECT_FILES_DIR must be set!"
+	exit 1
+fi
+
+project="HCP_Staging"
+subject_file_name="${SUBJECT_FILES_DIR}/${project}.CheckUpdateFixPackageCompletion.subjects"
+echo "Retrieving subject list from: ${subject_file_name}"
+subject_list_from_file=( $( cat ${subject_file_name} ) )
+subjects="`echo "${subject_list_from_file[@]}"`"
+
+for subject in ${subjects} ; do
+	if [[ ${subject} != \#* ]]; then
+		./CheckForUpdateFixPackageCompletion.sh --archive-root="/data/hcpdb/archive/${project}/arc001" --subject=${subject} --output-dir="/data/hcpdb/packages/PostMsmAll"
+	fi
+done
