500fa3b2230470b9805361a2755713e9296f3878
Timothy Brown <tbbrown@wustl.edu>
2015-12-15 11:45:38 -0600

    CheckForRestingStateStatsCompletion.Batch.sh removes previously generated output files

--- CompletionCheck_RestingStateStats/CheckForRestingStateStatsCompletion.Batch.sh
+++ CompletionCheck_RestingStateStats/CheckForRestingStateStatsCompletion.Batch.sh
@@ -11,8 +11,11 @@ echo "Retrieving subject list from: ${subject_file_name}"
 subject_list_from_file=( $( cat ${subject_file_name} ) )
 subjects="`echo "${subject_list_from_file[@]}"`"
 
+rm -f ${project}.complete.txt
+rm -f ${project}.incomplete.txt
+
 for subject in ${subjects} ; do
 	if [[ ${subject} != \#* ]]; then
-		./CheckForRestingStateStatsCompletion.sh --project=${project} --subject=${subject} # --details
+		./CheckForRestingStateStatsCompletion.sh --project=${project} --subject=${subject}  #--details
 	fi
 done
