d1c6b6cea90afd266481d6748fea2c12edfc957d
Timothy Brown <tbbrown@wustl.edu>
2016-08-24 13:31:26 -0500

    Moving completion checks to xnat_pbs_jobs repo

---
+++ CheckPackageExistence/Check500SubjectRelease/Check500SubjectRelease.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+subject_files_dir=~/subject_list_files
+subject_file_name="${subject_files_dir}/Full500SubjectsRelease.subjects"
+echo "Retrieving subject list from: ${subject_file_name}"
+subject_list_from_file=( $( cat ${subject_file_name} ) )
+subjects="`echo "${subject_list_from_file[@]}"`"
+
+rm -f Full900SubjectsRelease.out
+for subject in ${subjects} ; do
+	../CheckPackageExistence.sh --subject=${subject} --rootdir=/data/hcpdb/packages/prerelease/zip/HCP_500 | tee ${subject}.out
+	cat ${subject}.out >> Full500SubjectsRelease.out
+done
+
